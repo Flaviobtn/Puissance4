6c4993f0db6b3e6976db7e84c5edf7ad5f695481
ClemLeBoss <clementmoisan16@gmail.com>
2025-05-26 15:17:17 +0200

    modif joueur

--- code/src/Joueur.java
+++ code/src/Joueur.java
@@ -9,7 +9,28 @@ public class Joueur {
         this.couleur = couleur;
     }
 
-}
+    public String getPseudo() {
+        return pseudo;
+    }
+    public String getCouleur() {
+        return couleur;
+    }
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
 
-
+    public placerJetons(Grille grille, int colonne){
+        if (grille.placerJeton(this, colonne, couleur)) {
+            System.out.println("Jeton placé dans la colonne " + colonne);
+        } else {
+            System.out.println("Impossible de placer le jeton dans la colonne " + colonne);
+        }
+    }
+    @Override
+    public String toString() {
+        return "Joueur{" + "pseudo= " + pseudo + ", couleur= " + couleur + '}';
+    }
 }
