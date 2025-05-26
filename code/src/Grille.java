1c29dc8fa4b77c9f06d6897e99a0bbbd1a4ef200
Flaviobtn <flavio.bouton@etu.univ-orleans.fr>
2025-05-26 14:54:05 +0200

    Updates Jeton et Grille

---
+++ code/src/Grille.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Grille {
+    private int longueur;
+    private int largeur;
+
+    public Grille(int longueur, int largeur){
+        String[][] martice = new String[6][7];
+        for(String)
+    }
+}
---
+++ code/src/Jeton.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Jeton{
+    private String couleur;
+    private Integer x;
+    private Integer y;
+
+    public Jeton(){
+        this.couleur = "blanc";
+        this.x = null;
+        this.y = null;
+    }
+
+    public Jeton(String couleur){
+        this.couleur = couleur;
+        this.x = null;
+        this.y = null;
+    }
+
+    public String getCouleur(){
+        return this.couleur;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int gety(){
+        return this.y;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+}
