0d1b497a00f48a673b2dd222b85f9a8c96693eec
ClemLeBoss <clementmoisan16@gmail.com>
2025-05-26 15:03:04 +0200

    Joueur

--- code/src/Joueur.java
+++ code/src/Joueur.java
@@ -1,5 +1,21 @@
 import java.util.*;
 
 public class Joueur {
+    private String pseudo;
+    private String couleur;
+    private int numéroCarteBleue;
+    private int tailleDuZgeg;
+    private String religion;
+    private String abaLesNoirs;
+
+    public Joueur(String pseudo, String couleur, int numéroCarteBleue, int tailleDuZgeg, String religion, String abaLesNoirs) {
+        this.pseudo = pseudo;
+        this.couleur = couleur;
+        this.numéroCarteBleue = numéroCarteBleue;
+        this.tailleDuZgeg = tailleDuZgeg;
+        this.religion = religion;
+        this.abaLesNoirs = abaLesNoirs;
+    }
+
     
 }
